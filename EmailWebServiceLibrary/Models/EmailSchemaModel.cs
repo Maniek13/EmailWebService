e90fb0ded34bc0bfeecb97b12624cad5088422fe
Maniek13 <mani3k1989@gmail.com>
2024-02-27 20:57:21 +0100

    usuwanie

--- EmailWebServiceLibrary/Models/EmailSchemaModel.cs
+++ EmailWebServiceLibrary/Models/EmailSchemaModel.cs
@@ -7,15 +7,10 @@ namespace EmailWebServiceLibrary.Models
     {
         public int Id { get; set; }
         public int ServiceId { get; set; }
-        [Required(ErrorMessage = "Adress to is required")]
         public string From { get; set; }
-
         public string DisplayName { get; set; }
-        [Required(ErrorMessage = "Email schema name is required")]
         public string Name { get; set; }
-        [Required(ErrorMessage = "Email subject is required")]
         public string Subject { get; set; }
-        [Required(ErrorMessage = "Email bdy is required")]
         public string Body { get; set; }
         public string ReplyTo { get; set; }
         public string ReplyToDisplayName { get; set; }
